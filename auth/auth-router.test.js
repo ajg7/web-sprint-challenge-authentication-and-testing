7022a48d87c3b44f029c4776d3d76ff68fc0d2b6
ajg7 <ajgebara@gmail.com>
2020-10-16 12:34:41 -0500

    all tests pass

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,16 +1,32 @@
+const { expectCt } = require("helmet");
 const supertest = require("supertest");
 const server = require("../api/server");
 
 
 const user = {
-    username: "Peach",
+    username: "Peach3",
     password: "i<3Mario"
 }
+
+let token = "";
 
 describe("Auth-Router", () => {
     describe("Register endpoint", () => {
         it("allows users to register", () => {
-            
+            return supertest(server).post("/api/auth/register").send(user).then(response => {expect(response.body)})
+        })
+    })
+    describe("Login endpoint", () => {
+        it("Allows users to login", () => {
+            return supertest(server).post("/api/auth/login").send(user).then(response => {
+                expect(response.status).toBe(200);
+                token = response.body.token;
+            })
+        })
+    })
+    describe("Get Jokes", () => {
+        it("should have a status 200", () => {
+            return supertest(server).get("/api/jokes").set("Authorization", token).then(response => {expect(response.status).toBe(200)})
         })
     })
 })
