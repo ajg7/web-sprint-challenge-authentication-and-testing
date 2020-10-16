220252b7b6bb4ab1f83f80993652248f9a2893ca
ajg7 <ajgebara@gmail.com>
2020-10-16 11:28:00 -0500

    made register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,26 @@
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("./secrets");
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
+const Users = require("../users/model-users");
+const { isValid } = require("../jokes/jokes-service");
+const { response } = require("express");
+
+router.post('/register', (request, response) => {
   // implement registration
+  const credentials = request.body;
+  const rounds = process.env.BCRYPT_ROUNDS || 8;
+  const hash = bcryptjs.hashSync(credentials.password, rounds);
+  credentials.password = hash;
+
+  Users.add(credentials)
+    .then(user => {
+      response.status(201).json({ data: user })
+    })
+    .catch(error => {
+      response.status(500).json({ message: error.message })
+    })
 });
 
 router.post('/login', (req, res) => {
