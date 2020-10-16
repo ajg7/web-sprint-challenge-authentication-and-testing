447f79af8c61b03f17756894f331d683c2f75b8f
ajg7 <ajgebara@gmail.com>
2020-10-16 11:45:40 -0500

    successfully made login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,19 @@ const router = require('express').Router();
 
 const Users = require("../users/model-users");
 const { isValid } = require("../jokes/jokes-service");
-const { response } = require("express");
+
+function getJwt(user) {
+  const payload = {
+    username: user.username
+  }
+  const secret = secrets.jwtSecret;
+  const options = {
+    expiresIn: "10h"
+  }
+  return jwt.sign(payload, secret, options)
+}
+
+
 
 router.post('/register', (request, response) => {
   // implement registration
@@ -23,8 +35,27 @@ router.post('/register', (request, response) => {
     })
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', (request, response) => {
   // implement login
+  const { username, password } = request.body;
+
+  if (isValid(request.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        console.log(user)
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = getJwt(user);
+          response.status(200).json({ message: "Welcome to our API", token })
+        } else {
+          response.status(401).json({ message: "Invalid Characters" })
+        }
+      })
+      .catch(error => {
+        response.status(500).json({ message: error.message })
+      })
+  } else {
+    response.status(400).json({ message: "Please Provide username and password" })
+  }
 });
 
 module.exports = router;
