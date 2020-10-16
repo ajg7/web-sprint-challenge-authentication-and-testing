f9d5050e6bc91c753353f28bb983fa09118e5126
ajg7 <ajgebara@gmail.com>
2020-10-16 11:59:18 -0500

    added test file and put jest in scripts

---
+++ auth/auth-router.test.js
@@ -0,0 +1,16 @@
+const supertest = require("supertest");
+const server = require("../api/server");
+
+
+const user = {
+    username: "Peach",
+    password: "i<3Mario"
+}
+
+describe("Auth-Router", () => {
+    describe("Register endpoint", () => {
+        it("allows users to register", () => {
+            
+        })
+    })
+})
